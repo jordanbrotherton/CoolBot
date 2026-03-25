91aeb03b5dbb3b3f9a0e85f0f63611019260dadc
Jordan Brotherton <135299181+jordanbrotherton@users.noreply.github.com>
2026-03-24 23:45:57 -0400

    feat: Add custom status

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.Extensions.Hosting;
+using NetCord;
 
 using NetCord.Hosting.Gateway;
 using NetCord.Hosting.Services;
 using NetCord.Hosting.Services.ApplicationCommands;
 
+using NetCord.Gateway;
+
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services
-    .AddDiscordGateway()
+    .AddDiscordGateway(options =>
+    {
+        options.Presence = new PresenceProperties(UserStatusType.Online)
+        {
+            Activities = [new UserActivityProperties("Tracking your debts with a close eye...", UserActivityType.Watching)]
+        };
+    })
     .AddApplicationCommands();
 
 var host = builder.Build();
